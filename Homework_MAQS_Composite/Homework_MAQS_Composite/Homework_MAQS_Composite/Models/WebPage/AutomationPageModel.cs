6a1c7ad02870dd3b27f8e6b8d5f09c4d53719dfc
bmonzon8 <100320366+bmonzon8@users.noreply.github.com>
2022-02-24 11:36:42 -0800

    added sample test class and method

---
+++ Homework_MAQS_Composite/Homework_MAQS_Composite/Homework_MAQS_Composite/Models/WebPage/AutomationPageModel.cs
@@ -0,0 +1,110 @@
+﻿using CognizantSoftvision.Maqs.BaseSeleniumTest;
+using CognizantSoftvision.Maqs.BaseSeleniumTest.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using System;
+
+namespace Models
+{
+    /// <summary>
+    /// Page object for the Automation page
+    /// </summary>
+    public class AutomationPageModel : BaseSeleniumPageModel
+    {
+        /// <summary>
+        /// The page url
+        /// </summary>
+        private static string PageUrl = SeleniumConfig.GetWebSiteBase() + "Automation";
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LoginPageModel" /> class.
+        /// </summary>
+        /// <param name="testObject">The test object</param>
+        public AutomationPageModel(ISeleniumTestObject testObject) : base(testObject)
+        {
+        }
+
+        /// <summary>
+        /// Gets user name box
+        /// </summary>
+        private LazyElement GetShowDialog
+        {
+            get { return this.GetLazyElement(By.CssSelector("#showDialog"), "Show Dialog button"); }
+        }
+
+        /// <summary>
+       
+
+       
+        /// <summary>
+        /// Open the login page
+        /// </summary>
+        public void OpenAutomationPage()
+        {
+            this.TestObject.WebDriver.Navigate().GoToUrl(PageUrl);
+            this.AssertPageLoaded();
+        }
+
+
+        /// <summary>
+        /// Assert the login page loaded
+        /// </summary>
+        public void AssertPageLoaded()
+        {
+            Assert.IsTrue(
+                this.IsPageLoaded(),
+                "The web page '{0}' is not loaded",
+                PageUrl);
+        }
+
+        /// <summary>
+        /// Check if the home page has been loaded
+        /// </summary>
+        /// <returns>True if the page was loaded</returns>
+        public override bool IsPageLoaded()
+        {
+            return this.ShowDialogButton.Displayed;
+        }
+
+        /// <summary>
+        /// Gets Show Dialog button
+        /// </summary>
+        private LazyElement ShowDialogButton
+        {
+            get { return this.GetLazyElement(By.CssSelector("#showDialog1"), "Show Dialog button"); }
+        }
+
+        /// <summary>
+        /// Clicks on a button
+        /// </summary>
+        private void ClickButton(LazyElement lazyElementIn)
+        {
+            try
+            {
+                LazyElement buttonToClick = lazyElementIn;
+                lazyElementIn.Click();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The Exception Details are: " + e.Message);
+            }
+            finally { }
+        }
+
+        public void ClickShowDialogButton()
+        {
+            try
+            {
+                LazyElement buttonToClick = ShowDialogButton;
+                this.ClickButton(buttonToClick);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The Exception Details are: " + e.Message);
+            }
+            finally { }
+        }
+    }
+}
+
---
+++ Homework_MAQS_Composite/Homework_MAQS_Composite/Homework_MAQS_Composite/Tests/Homework/Homework_SeleniumTests.cs
@@ -0,0 +1,91 @@
+using CognizantSoftvision.Maqs.BaseDatabaseTest;
+using CognizantSoftvision.Maqs.BaseSeleniumTest;
+using CognizantSoftvision.Maqs.BaseWebServiceTest;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Models;
+using System;
+using System.Data;
+using System.Linq;
+
+namespace Tests
+{
+    /// <summary>
+    /// Composite Selenium test class
+    /// </summary>
+    [TestClass]
+    public class Homework_SeleniumTests : BaseSeleniumTest
+    {
+        /// <summary>
+        /// Do database setup for test run
+        /// </summary>
+        // [ClassInitialize] - Disabled because this step will fail as the template does not include access to a test database
+        public static void TestSetup(TestContext context)
+        {
+            // Do database setup
+            using (DatabaseDriver wrapper = new DatabaseDriver(DatabaseConfig.GetProviderTypeString(), DatabaseConfig.GetConnectionString()))
+            {
+                var result = wrapper.Query("getStateAbbrevMatch", new { StateAbbreviation = "MN" }, commandType: CommandType.StoredProcedure);
+                Assert.AreEqual(1, result.Count(), "Expected 1 state abbreviation to be returned.");
+            }
+        }
+        
+
+        /// <summary>
+        /// Do post test run web service cleanup
+        /// </summary>
+        [ClassCleanup]
+        public static void TestCleanup()
+        {
+            // Do web service post run cleanup
+            WebServiceDriver client = new WebServiceDriver(new Uri(WebServiceConfig.GetWebServiceUri()));
+            string result = client.Delete("/api/String/Delete/1", "text/plain", true);
+            Assert.AreEqual(string.Empty, result);
+        }
+
+        /// <summary>
+        /// Open page test
+       /// Did the Automation Page Load
+        /// </summary>
+        [TestMethod]
+        public void OpenAutomationPageTest()
+        { 
+            try
+            {
+                AutomationPageModel page = new AutomationPageModel(this.TestObject);
+                page.OpenAutomationPage();
+                page.AssertPageLoaded();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The Exception details are:" + e.Message);
+                throw;
+            }
+            finally { }
+
+
+        }
+
+       
+        [TestMethod]
+        public void VerifyShowDialogButton()
+        {
+            try
+            {
+                AutomationPageModel page = new AutomationPageModel(this.TestObject);
+                page.OpenAutomationPage();
+                page.ClickShowDialogButton();
+                
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The Exception details are:" + e.Message);
+                throw;
+            }
+            finally { }
+
+
+        }
+
+    }
+}
